064935cc99b32be49ca3943bf1155f638d00be2e
S-Lombardi <silo.lombardi@gmail.com>
2023-08-24 20:20:14 +0200

    Aggunti commenti

--- resources/views/admin/works/create.blade.php
+++ resources/views/admin/works/create.blade.php
@@ -17,7 +17,7 @@
                     </div>
                 @endif --}}
     
-    
+            {{---- TABELLA PORTFOLIOS ----}}
                 {{-- INIZIO FORM --}}
                 <form action="{{route('admin.works.store')}}" method="POST" enctype="multipart/form-data">
                     @csrf
@@ -26,55 +26,56 @@
                         <label for="" class="control-label">Titolo</label>
                         <input type="text"class="form-control" id="title" name="title" placeholder="titolo">
                     </div>
-    
                     {{-- IMG --}}
                     <div class="form-group">
                         <label for="" class="control-label">Immagine</label>
                         <input type="file" class="form-control" id="image" name="image" placeholder="Immagine progetto">
                     </div>
-    
                     {{-- LINK --}}
                     <div class="form-group">
                         <label for="" class="control-label">Link al progetto</label>
                         <input type="text"class="form-control" id="link" name="link" placeholder="link">
                     </div>
-    
                     {{-- DESCRIZIONE --}}
                     <div class="form-group">
                         <label for="" class="control-label">Descrizone</label>
                         <input type="text"class="form-control" id="description" name="description" placeholder="Inserisci descrizione...">
                     </div>
-    
                     {{-- FIGURE PROFESSIONALI --}}
                     {{-- Back-ender --}}
                     <div class="form-group">
                         <label for="" class="control-label">Nome back-ender</label>
                         <input type="text"class="form-control" id="back_ender" name="back_ender" placeholder="back_ender">
                     </div>
-    
                     {{-- Front-ender --}}
                     <div class="form-group">
                         <label for="" class="control-label">Nome front-ender</label>
                         <input type="text"class="form-control" id="front_ender" name="front_ender" placeholder="front_ender">
                     </div>
-    
                     {{-- UX --}}
                     <div class="form-group">
                         <label for="" class="control-label">Nome UX</label>
                         <input type="text"class="form-control" id="ux" name="ux" placeholder="ux">
                     </div>
-    
                     {{-- UI --}}
                     <div class="form-group">
                         <label for="" class="control-label">Nome UI</label>
                         <input type="text"class="form-control" id="ui" name="ui" placeholder="ui">
                     </div>
-    
                     {{-- Illustratore --}}
                     <div class="form-group">
                         <label for="" class="control-label">Nome Illustratore</label>
                         <input type="text"class="form-control" id="illustrator" name="illustrator" placeholder="illustrator">
                     </div>
+                {{---- FINE TABELLA PORTFOLIOS ----}}
+
+                {{---- TABELLA TYPES SELECT ----}}
+                    <div class="form-control">
+                        <select name="" id="">
+                            
+                        </select>
+                    </div>
+                {{---- FINE TABELLA TYPES SELECT ----}}
     
                     {{-- PULSANTE --}}
                     <div class="form-group">
